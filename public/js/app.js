7d5a7b7058fb7adbeeb0cb966813af636cf7c703
Ankur Singh <Ankur@Ankurs-MacBook-Pro.local>
2016-01-02 21:11:25 +0530

    Login and singup page correction

--- public/js/app.js
+++ public/js/app.js
@@ -2,27 +2,28 @@ var app = angular.module('peopleware', ['ui.router']);
 
 app.config(function($stateProvider, $urlRouterProvider) {
 
+  $urlRouterProvider.otherwise('/');
   $stateProvider
     .state('signup', {
       url: "/",
-      templateUrl: "../views/signup.html"
+      templateUrl: "views/signup.html"
     })
     .state('login', {
       url: "/login",
-      templateUrl: "../views/login.html"
+      templateUrl: "/views/login.html"
     })
     .state('home', {
       url: "/home",
-      templateUrl: "../views/home.html"
+      templateUrl: "/views/home.html"
     })
     .state('campaign', {
       url: "/campaign",
-      templateUrl: "../views/campaign.html"
+      templateUrl: "/views/campaign.html"
     })
     .state('profile', {
       url: "/profile",
-      templateUrl: "../views/profile.html"
+      templateUrl: "/views/profile.html"
     });
 
-  $urlRouterProvider.otherwise('home');
+
 });
